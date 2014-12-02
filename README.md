df34e893ce1107f0229216735f383f9ca20c9e66
Josh Gillies <github@joshgilli.es>
2014-12-02 13:23:57 +1100

    Update API. Evented API and recursive checking

--- README.md
+++ README.md
@@ -1,30 +1,26 @@
 # Knoq
 
-A general purpose resource checker.
+A general purpose continuous web resource checker.
 
 With Knoq in the same way you'd knock on a door
 to see who's home you can Knoq a web resource to
 see what's shaking.
 
-If you can curl it you can Knoq it.
-
 # Example
 
 ```javascript
 var Knoq = require('knoq');
-var resource = Knoq('www.google.com/somepath');
+var resource = Knoq('http://www.google.com/');
 
-setInterval(function() {
-  resource.response(function(err, res) {
-    // err is returned if resource hasn't been updated since we last checked.
-    if (err) return console.log(err);
+resource.on('response', function(res) {
+  console.log('EventEmitter');
+  console.log(res.statusCode);
+  mountain.end();
+});
 
-    // res is returned when the resource has been updated.
-    // res is the same Readable Stream you'd recieve from http.request
-    var modified = new Date(res.headers['last-modified']);
-    console.log('Resource updated! See: ', modified);
-  });
-}, 60 * 1000);
+resource.on('end', function() {
+  console.log('ended!');
+});
 ```
 
 # Install
--- index.js
+++ index.js
@@ -1,25 +1,81 @@
-var url = require('url');
-var path = require('path');
-var cheqr = require(path.join(__dirname, 'lib', 'cheqr.js'));
+var inherits = require('inherits');
 var hyperquest = require('hyperquest');
+var EventEmitter = require('events').EventEmitter;
 
-function Knoq(opts) {
-  if (!(this instanceof Knoq)) return new Knoq(opts);
-  if (typeof opts === 'string') opts = url.parse(opts);
-  if (!opts) opts = {};
-  this.options = opts;
-  this.lastModified = null;
-}
+var Knoq = function Knoq(uri, opts, callback) {
+  if (!(this instanceof Knoq)) return new Knoq(uri, opts, callback);
+  if (typeof uri === 'object') {
+    callback = opts;
+    opts = uri;
+    uri = undefined;
+  }
+  if (typeof opts === 'number') {
+    opts = {
+      delay: opts
+    };
+  }
+  if (typeof opts === 'function') {
+    callback = opts;
+    opts = undefined;
+  }
+  if (!opts) {
+    opts = {
+      delay: 60000
+    };
+  }
+  if (uri !== undefined) opts.uri = uri;
 
-Knoq.prototype.response = function(callback) {
-  cheqr.call(this, function(err) {
-    if (err) return callback(err, null);
-    hyperquest(this.options, function(res) {
-      return callback(null, res);
-    }).on('error', function(err) {
-      return callback(err, null);
-    }).end();
+  var self = this;
+
+  EventEmitter.call(this);
+
+  this.uri = opts.uri;
+  this.pending = false;
+  this.running = true;
+
+  this.interval = setInterval(function() {
+    self.preRequest();
+  }, opts.delay);
+
+  if (callback) {
+    this.on('error', function(err) {
+      callback(err);
+    });
+    this.on('response', function(res) {
+      callback(null, res);
+    });
+  }
+};
+
+inherits(Knoq, EventEmitter);
+
+Knoq.prototype.preRequest = function preRequest() {
+  if (!this.pending && this.running)
+    this.request();
+};
+
+Knoq.prototype.request = function request() {
+  var self = this;
+
+  self.pending = true;
+
+  var req = hyperquest(self.uri);
+  req.on('response', function(res) {
+    self.pending = false;
+    self.emit('response', res);
   });
+  req.on('error', function(err) {
+    self.emit('error', err);
+    self.end();
+  });
+
+};
+
+Knoq.prototype.end = function end() {
+  if (this.interval) clearInterval(this.interval);
+  this.pending = false;
+  this.running = false;
+  this.emit('end');
 };
 
 module.exports = Knoq;
